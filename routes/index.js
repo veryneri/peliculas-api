e160d4b270a201c1fb4033210c1404e4b05d530e
Edson Neri <edson.neri@correo.uia.mx>
2017-09-26 14:37:31 -0500

    add routes

---
+++ routes/index.js
@@ -0,0 +1,14 @@
+"use strict"
+const express = require('express');
+const router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res
+    .status(200)
+    .json({
+      message: 'hola mundo'
+    });
+});
+
+module.exports = router;
